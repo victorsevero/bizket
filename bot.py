dee336c03f240fe0726c8d0301ce2dc4d9991471
Victor Severo <victor.severo.miranda@gmail.com>
2022-10-25 01:09:31 -0300

    fix: 🐛 fix model saving

--- bot.py
+++ bot.py
@@ -1,26 +1,14 @@
+import os
 import pickle
+from datetime import datetime
+from copy import deepcopy
 
 import numpy as np
-
-import matplotlib.pyplot as plt
 import pygad
 import pygad.nn
 import pygad.gann
 
 from server import Server
-
-
-gann = pygad.gann.GANN(
-    num_solutions=10,
-    num_neurons_input=4,
-    num_neurons_hidden_layers=[5],
-    num_neurons_output=5,
-    hidden_activations="relu",
-    output_activation="softmax",
-)
-
-server = Server()
-server.load_state()
 
 
 def buttons_map(buttons):
@@ -70,29 +58,39 @@ def msg_to_nn_inputs(data):
     return data_inputs
 
 
-def fitness_func(solution, sol_idx):
-    global gann, server, best_solution, best_solution_fitness
+def game_loop(last_layer):
+    global server
 
     server.load_state()
-    done = False
-    i = 0
-    while (not done) and i < 60 * 60:
+    for _ in range(60 * 20):
         data = server.get_msg()
         data_inputs = msg_to_nn_inputs(data)
         buttons = pygad.nn.predict(
-            last_layer=gann.population_networks[sol_idx],
+            last_layer=last_layer,
             data_inputs=data_inputs,
         )
         joypad_msg = buttons_map(buttons)
         server.send_msg(joypad_msg)
-        done = (data["player_hp"] == 0) or (data["boss_hp"] == 0)
-        i += 1
+        if (data["player_hp"] == 0) or (data["boss_hp"] == 0):
+            break
 
+    return data
+
+
+def calc_fitness(data):
+    return data["player_hp"] - data["boss_hp"]
+
+
+def fitness_func(solution, sol_idx):
+    global gann, server, best_solution, best_fitness
+
+    data = game_loop(gann.population_networks[sol_idx])
     print(f"{sol_idx} ", end="")
 
-    fitness = data["player_hp"] - data["boss_hp"]
-    if fitness > best_solution_fitness:
-        best_solution = gann.population_networks[sol_idx]
+    fitness = calc_fitness(data)
+    if fitness > best_fitness:
+        best_fitness = fitness
+        best_solution = deepcopy(gann.population_networks[sol_idx])
     return fitness
 
 
@@ -116,12 +114,13 @@ def callback_generation(ga):
 
 
 def bot():
+    global best_solution
     population_vectors = pygad.gann.population_as_vectors(
         population_networks=gann.population_networks
     )
 
     ga = pygad.GA(
-        num_generations=200,
+        num_generations=10,
         num_parents_mating=2,
         fitness_func=fitness_func,
         initial_population=population_vectors.copy(),
@@ -130,14 +129,55 @@ def bot():
         save_best_solutions=True,
     )
 
-    best_solution = None
-    best_solution_fitness = -49
     ga.run()
-    ga.plot_fitness(save_dir="fitness.png")
-    ga.save("model")
-    with open("nn.pkl", "wb") as fp:
+    now_str = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    results_dir = f"results/{now_str}"
+    os.makedirs(results_dir)
+    ga.plot_fitness(save_dir=f"{results_dir}/fitness.png")
+    with open(f"{results_dir}/model.pkl", "wb") as fp:
         pickle.dump(best_solution, fp)
 
 
+def reshape_weights(weights):
+    i = gann.num_neurons_input
+    j = gann.num_neurons_hidden_layers[0]
+    k = gann.num_neurons_output
+
+    w1 = weights[: i * j].reshape(i, j)
+    w2 = weights[i * j :].reshape(j, k)
+    return [w1, w2]
+
+
+def list_dirs(path="results/"):
+    return [path + x for x in os.listdir(path) if os.path.isdir(path + x)]
+
+
+def run_saved_model(filename=None):
+    if filename is None:
+        filename = sorted(list_dirs())[-1] + "/model.pkl"
+
+    with open(filename, "rb") as fp:
+        nn = pickle.load(fp)
+
+    data = game_loop(nn)
+    print(calc_fitness(data))
+
+
 if __name__ == "__main__":
+    gann = pygad.gann.GANN(
+        num_solutions=10,
+        num_neurons_input=4,
+        num_neurons_hidden_layers=[5],
+        num_neurons_output=5,
+        hidden_activations="relu",
+        output_activation="softmax",
+    )
+
+    server = Server()
+    best_solution = None
+    best_fitness = -49
+
     bot()
+    # run_saved_model()
+
+# list 10 lists 2 arrays (4, 5), (5, 5)
